9a2eb0294bc6ea1bd3c0831b58ca3cb830f66def
Viteira <jhonnvicttorr@gmail.com>
2024-04-20 01:25:13 -0300

    feat: adicionado componente de template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, CommonModule, TemplateBindingComponent],
+  template: `
+    <app-template-binding />
+  `
 })
 export class AppComponent {
-  title = 'primeiroProjeto';
+  title = 'Angular 17';
 }
