76090d77b32ada45a60082459cb74d8a6de31524
Viteira <jhonnvicttorr@gmail.com>
2024-04-20 01:25:33 -0300

    feat: adicionado componente para teste

---
+++ src/app/components/meu-component/meu-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './meu-component.component.html',
+  styleUrl: './meu-component.component.scss'
+})
+export class MeuComponentComponent {
+
+}
